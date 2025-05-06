04c032651beca911a48ab7d2a6df0fc9ba08aa42
Thiago Mota <thiagomotasilva2000@gmail.com>
2025-05-06 20:06:16 -0300

    feat: update TabManager to use client-side routing and improve navigation styling

--- src/components/tab-manager.tsx
+++ src/components/tab-manager.tsx
@@ -1,28 +1,55 @@
-import { getTranslations } from "next-intl/server";
-import { Link } from "@/i18n/navigation";
+"use client";
 
-export async function TabManager() {
-  const t = await getTranslations('navigation');
+import { usePathname, Link } from "@/i18n/navigation";
+import { clsx } from "clsx";
+import { useTranslations } from "next-intl";
+import { useCallback, useState } from "react";
+
+export function TabManager() {
+  const t = useTranslations('navigation');
+  const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => setOpen(prev => !prev), []);
 
   const routes = [
-    { label: t('home'), route: '' },
-    { label: t('about'), route: 'about' },
-    { label: t('projects'), route: 'projects' },
-    { label: t('contact'), route: 'contact' },
+    { label: t('home'), path: '/' },
+    { label: t('about'), path: '/about' },
+    { label: t('projects'), path: '/projects' },
+    { label: t('contact'), path: '/contact' },
   ];
 
   return (
-    <nav className="flex w-full font-mono border rounded-t-2xl">
-      <ul className="flex w-full items-center">
-        <li className="p-6 pr-16 border-r">{t('title')}</li>
-        {routes.map(route => (
-          <li key={route.route} className="inline-flex border-r last:border-l last:ml-auto">
-            <Link href={`/${route.route}`} className="p-6 hover:brightness-75 transition-all duration-150">
-              {route.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="flex w-full font-mono border rounded-t-2xl justify-between items-center relative sm:justify-start">
+      <Link className="p-6 border-r" href="/">{t('title')}</Link>
+
+      <button type="button" onClick={toggleOpen}
+              className="cursor-pointer inline-flex items-center mr-2 p-2 w-10 h-10 justify-center text-sm text-primary rounded-lg sm:hidden hover:bg-accent focus:outline-none focus:ring-2"
+              aria-controls="navbar-default" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+      </button>
+
+      <div className={clsx("absolute bottom-0 right-0 translate-y-full border bg-background sm:flex sm:grow sm:static sm:translate-y-0", { "hidden": !open })}>
+        <ul className="flex flex-col w-full items-center sm:flex-row">
+          {routes.map(route => (
+            <li
+              key={route.path}
+              className={clsx(
+                "inline-flex w-full sm:w-auto sm:border-r hover:bg-accent sm:last:ml-auto sm:last:border-l",
+                pathname === route.path ? "bg-accent" : "bg-background"
+              )}
+            >
+              <Link href={route.path} className="w-full p-6 hover:brightness-75 transition-all duration-150" onClick={toggleOpen}>
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   )
 }
--- src/components/under-construction.tsx
+++ src/components/under-construction.tsx
@@ -10,7 +10,7 @@ export async function UnderConstruction() {
     <div className="flex flex-col grow items-center justify-center h-full">
       <h1 className="text-4xl font-bold mb-4">{t('title')}</h1>
       <p className="text-lg mb-8">{t('description')}</p>
-      <Image alt={t('title')} src={underConstructionImage} className="rounded-lg shadow-lg max-w-[80vh]" />
+      <Image alt={t('title')} src={underConstructionImage} className="rounded-lg shadow-lg md:max-w-[80vh]" />
     </div>
   )
 }
